bf5f46c91acaa7a685e45889983db1264d479cd7
Victor Nicolet <victor.nicolet@polytechnique.edu>
2017-11-13 10:16:56 -0500

    Testing new input for Project 1: test passed.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -6,9 +6,21 @@ import minic.c_ast_to_minic as ctoc
 import minic.minic_ast as mast
 
 
-class TestConversion1(unittest.TestCase):
+class TestLoopAnalysis(unittest.TestCase):
     def test_reachingdefs(self):
         ast = ctoc.minic_parse_file('./c_files/loop_analysis.c')
+        # Reaching Definitions analysis
+        rdefs_analyzer = mana.FuncBodiesAnalysis('ReachingDefinitions')
+        rdefs_analyzer.visit(ast)
+
+        # Reaching Definitions analysis
+        liveness_analyzer = mana.FuncBodiesAnalysis('LiveVariables')
+        liveness_analyzer.visit(ast)
+
+
+class TestNestedLoops(unittest.TestCase):
+    def test_analyze(self):
+        ast = ctoc.minic_parse_file('./c_files/nested_loops.c')
         # Reaching Definitions analysis
         rdefs_analyzer = mana.FuncBodiesAnalysis('ReachingDefinitions')
         rdefs_analyzer.visit(ast)
@@ -18,3 +30,4 @@ class TestConversion1(unittest.TestCase):
         liveness_analyzer = mana.FuncBodiesAnalysis('LiveVariables')
         liveness_analyzer.visit(ast)
         liveness_analyzer.print_results()
+
